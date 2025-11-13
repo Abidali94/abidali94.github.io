7e10271472c1e657f84eddf16562526b561aa125
kharchasaathi <abidalimohammad94@gmail.com>
2025-11-13 19:20:04 +0530

    Update types.js

--- js/types.js
+++ js/types.js
@@ -1,26 +1,30 @@
-console.log("types.js module loaded");
+console.log("types.js loaded");
 
 window.renderTypes = function () {
-  const container = document.getElementById("typesContainer");
-
+  const box = document.getElementById("typesContainer");
   let list = JSON.parse(localStorage.getItem("types-data") || "[]");
 
-  container.innerHTML = `
-    <h3>Your Product Types</h3>
+  box.innerHTML = `
+    <h3>Product Types</h3>
     <ul>${list.map(t => `<li>${t}</li>`).join("")}</ul>
-    <input id="newType" placeholder="New type" />
+    <input id="newType" placeholder="New Type">
     <button onclick="addType()">Add</button>
   `;
 };
 
 window.addType = function () {
   let list = JSON.parse(localStorage.getItem("types-data") || "[]");
-  const newType = document.getElementById("newType").value;
+  const t = newType.value.trim();
 
-  if (newType.trim() !== "") {
-    list.push(newType);
-    localStorage.setItem("types-data", JSON.stringify(list));
-    cloudSave("types", list);
-    renderTypes();
-  }
+  if (!t) return alert("Enter type!");
+
+  list.push(t);
+  localStorage.setItem("types-data", JSON.stringify(list));
+
+  cloudSave("types", list);
+  renderTypes();
+};
+
+window.moduleInit = id => {
+  if (id === "types") renderTypes();
 };
